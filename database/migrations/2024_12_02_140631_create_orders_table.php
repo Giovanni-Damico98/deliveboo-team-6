2a8ca102bdc8fac7ac23d72293f3f367d62f5eea
Francesco-Matteucci <francescomatteucci31@gmail.com>
2024-12-02 15:25:53 +0100

    added migration_branch

---
+++ database/migrations/2024_12_02_140631_create_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id()->foreign('Restaurants.id');
+            $table->bigInteger('restaurant_id');
+            $table->decimal('total_price', 8, 2);
+            $table->string('firstname', 255);
+            $table->string('lastname', 255);
+            $table->string('address', 255);
+            $table->string('phone_number', 255);
+            $table->text('note');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};

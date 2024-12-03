479042494a6c6f24b6dafd4c1b4db8a902611057
Giovanni-Damico98 <gd.installazioni25@gmail.com>
2024-12-03 19:24:32 +0100

    add style for login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,84 +1,49 @@
 @extends('layouts.app')
-{{-- collego il file login.css per l'estetica di home.blade.php --}}
+{{-- collego il file login.css per l'estetica di login.blade.php --}}
 @vite('resources/css/login.css')
 {{-- collego il file login.js per le animazioni di login.blade.php --}}
-@vite('resources/js/home.js')
+@vite('resources/js/login.js')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Login') }}</div>
+    <section class="section-container">
+        <div class="login-container">
+            <div class="circle circle-one"></div>
+            <div class="form-container">
+                <img src="https://raw.githubusercontent.com/hicodersofficial/glassmorphism-login-form/master/assets/illustration.png"
+                    alt="illustration" class="illustration" />
+                <h1 class="opacity">LOGIN</h1>
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
 
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('login') }}">
-                            @csrf
+                    <input type="text" id="email" placeholder="EMAIL" name="email"
+                        class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}" required
+                        autocomplete="email" autofocus>
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
 
-                            <div class="row mb-3">
-                                <label for="email"
-                                    class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                    <input type="password" id="password" placeholder="PASSWORD" name="password"
+                        class="form-control @error('password') is-invalid @enderror" required
+                        autocomplete="current-password">
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
 
-                                <div class="col-md-6">
-                                    <input id="email" type="email"
-                                        class="form-control @error('email') is-invalid @enderror" name="email"
-                                        value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                    @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="row mb-3">
-                                <label for="password"
-                                    class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="password" type="password"
-                                        class="form-control @error('password') is-invalid @enderror" name="password"
-                                        required autocomplete="current-password">
-
-                                    @error('password')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="row mb-3">
-                                <div class="col-md-6 offset-md-4">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" name="remember" id="remember"
-                                            {{ old('remember') ? 'checked' : '' }}>
-
-                                        <label class="form-check-label" for="remember">
-                                            {{ __('Remember Me') }}
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="row mb-0">
-                                <div class="col-md-8 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('Login') }}
-                                    </button>
-
-                                    @if (Route::has('password.request'))
-                                        <a class="btn btn-link" href="{{ route('password.request') }}">
-                                            {{ __('Forgot Your Password?') }}
-                                        </a>
-                                    @endif
-                                </div>
-                            </div>
-                        </form>
-                    </div>
+                    <button class="opacity" type="submit">ACCEDI</button>
+                </form>
+                <div class="register-forget opacity">
+                    <a href="{{ route('register') }}">REGISTRATI</a>
+                    @if (Route::has('password.request'))
+                        <a href="{{ route('password.request') }}">RECUPERA PASSWORD</a>
+                    @endif
                 </div>
             </div>
+            <div class="circle circle-two"></div>
         </div>
-    </div>
+        <div class="theme-btn-container"></div>
+    </section>
 @endsection

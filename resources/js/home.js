68ed454d6e1e505f86fab5dbabaecbaf57841fdd
Giovanni-Damico98 <gd.installazioni25@gmail.com>
2024-12-03 18:47:51 +0100

    add media query mobile for home.blade.php

--- resources/js/home.js
+++ resources/js/home.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const title = document.querySelector(".title");
     const bodyText = document.getElementById("body-text");
 
-    // imposta le posizioni iniziali fuori dalla finestra
+    // Imposta le posizioni iniziali fuori dalla finestra
     title.style.position = "absolute";
     title.style.left = "-100%"; // parte da fuori dello schermo a sinistra
     title.style.opacity = "0";
@@ -16,11 +16,12 @@ document.addEventListener("DOMContentLoaded", () => {
     bodyText.style.transition =
         "right 1s ease-out 0.7s, opacity 1s ease-out 0.1s";
 
-    // avvia l'animazione quando la pagina è pronta
+    // Avvia l'animazione quando la pagina è pronta
     setTimeout(() => {
         title.style.left = "50%"; // centrato
         title.style.transform = "translateX(-50%) translatey(-200%)";
         title.style.opacity = "1";
+        title.classList.add("adjust-text-spacing"); // aggiungo una classe per media query
 
         bodyText.style.right = "50%"; // centrato
         bodyText.style.transform = "translateX(50%) translatey(-40%)";

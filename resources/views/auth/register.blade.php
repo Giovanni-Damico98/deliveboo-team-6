2deefa6797b0f889dc398ea117be46cecafa709e
Giovanni-Damico98 <gd.installazioni25@gmail.com>
2024-12-03 21:27:57 +0100

    style and responsive layout for register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,122 +1,106 @@
 @extends('layouts.app')
+{{-- collego il file register.css per l'estetica di register.blade.php --}}
+@vite('resources/css/register.css')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+    <div class="register-container">
+        <!-- Titolo della sezione -->
+        <div class="title">Registrazione</div>
+        <div class="register-content">
+            <!-- Form di registrazione -->
+            <form method="POST" action="{{ route('register') }}">
+                @csrf
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <!-- Dati per l'utente -->
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <!-- Dati per il suo ristorante -->
-                        <hr>
-                        <h5 class="text-center mb-3">Dati Attività</h5>
-                        <div class="row mb-3">
-                            <label for="restaurant_name" class="col-md-4 col-form-label text-md-end">Nome dell'Attività</label>
-                            <div class="col-md-6">
-                                <input id="restaurant_name" type="text" class="form-control @error('restaurant_name') is-invalid @enderror" name="restaurant_name" value="{{ old('restaurant_name') }}" required>
-                                @error('restaurant_name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="address" class="col-md-4 col-form-label text-md-end">Indirizzo dell'Attività</label>
-                            <div class="col-md-6">
-                                <input id="address" type="text" class="form-control @error('address') is-invalid @enderror" name="address" value="{{ old('address') }}" required>
-                                @error('address')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="vat_number" class="col-md-4 col-form-label text-md-end">Partita IVA</label>
-                            <div class="col-md-6">
-                                <input id="vat_number" type="text" class="form-control @error('vat_number') is-invalid @enderror" name="vat_number" value="{{ old('vat_number') }}" required>
-                                @error('vat_number')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="image" class="col-md-4 col-form-label text-md-end">Immagine Ristorante -URL-</label>
-                            <div class="col-md-6">
-                                <input id="image" type="url" class="form-control @error('image') is-invalid @enderror" name="image" value="{{ old('image') }}">
-                                @error('image')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                <!-- Dati dell'utente -->
+                <div class="user-details">
+                    <div class="input-box">
+                        <span class="details">Nome</span>
+                        <input id="name" type="text" class="@error('name') is-invalid @enderror" name="name"
+                            value="{{ old('name') }}" placeholder="Inserisci il nome" required autocomplete="name"
+                            autofocus>
+                        @error('name')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="input-box">
+                        <span class="details">E-mail</span>
+                        <input id="email" type="email" class="@error('email') is-invalid @enderror" name="email"
+                            value="{{ old('email') }}" placeholder="Inserisci la mail" required autocomplete="email">
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="input-box">
+                        <span class="details">Password</span>
+                        <input id="password" type="password" class="@error('password') is-invalid @enderror"
+                            name="password" placeholder="Inserisci la password" required autocomplete="new-password">
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="input-box">
+                        <span class="details">Conferma Password</span>
+                        <input id="password-confirm" type="password" name="password_confirmation"
+                            placeholder="Conferma la password" required autocomplete="new-password">
+                    </div>
                 </div>
-            </div>
+
+                <!-- Dati del ristorante -->
+                <div class="user-details">
+                    <div class="input-box">
+                        <span class="details">Nome dell'Attività</span>
+                        <input id="restaurant_name" type="text" class="@error('restaurant_name') is-invalid @enderror"
+                            name="restaurant_name" value="{{ old('restaurant_name') }}" placeholder="Nome dell'attività"
+                            required>
+                        @error('restaurant_name')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="input-box">
+                        <span class="details">Indirizzo dell'Attività</span>
+                        <input id="address" type="text" class="@error('address') is-invalid @enderror" name="address"
+                            value="{{ old('address') }}" placeholder="Indirizzo dell'attività" required>
+                        @error('address')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="input-box">
+                        <span class="details">Partita IVA</span>
+                        <input id="vat_number" type="text" class="@error('vat_number') is-invalid @enderror"
+                            name="vat_number" value="{{ old('vat_number') }}" placeholder="Partita IVA" required>
+                        @error('vat_number')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="input-box">
+                        <span class="details">Immagine Ristorante - URL</span>
+                        <input id="image" type="url" class="@error('image') is-invalid @enderror" name="image"
+                            value="{{ old('image') }}" placeholder="Inserisci URL">
+                        @error('image')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                </div>
+
+                <!-- Pulsante di registrazione -->
+                <div class="button">
+                    <input type="submit" value="Registrati">
+                </div>
+            </form>
         </div>
     </div>
-</div>
 @endsection

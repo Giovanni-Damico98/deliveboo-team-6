a533b78e3b5316362e5e973f7a7cea927fca6443
Giovanni-Damico98 <gd.installazioni25@gmail.com>
2024-12-04 11:49:37 +0100

    add card in dashboard.blade

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,9 +1,24 @@
 @extends('layouts.admin')
-
+@vite('resources/css/admin/dashboard.css')
 @section('content')
-    <h1>Benvenuto nella dashboard del ristorante {{ $restaurant->name }}</h1>
-    <p> Indirizzo: {{ $restaurant->address }} </p>
-    <p> P.iva: {{ $restaurant->vat_number }} </p>
-    <p> <img src="{{ $restaurant->image }}" alt="{{ $restaurant->name }}"></p>
-    <p>Organizza i tuoi piatti, i tuoi ordini e le statistiche dal menù in alto</p>
+    <div class="restaurant-card" style="background-image: url('{{ asset($restaurant->image) }}');">
+        <div class="restaurant-card-overlay">
+            <div class="restaurant-card-content">
+                <h1 class="restaurant-title m-2 fs-1">{{ $restaurant->name }}</h1>
+                <p class="restaurant-info mt-5">
+                    Organizza i tuoi piatti, i tuoi ordini e le statistiche
+                </p>
+                <div class="link-div d-flex justify-content-around mt-3">
+                    <a class="link" href="{{ route('admin.dishes.index') }}">Piatti</a>
+                    <a class="link" href="#{{-- {{ route('admin.orders.index') }} --}}">Ordini</a>
+                    <a class="link" href="#{{-- {{ route('admin.statistics') }} --}}">Statistiche</a>
+                </div>
+
+                <div class="card-info d-flex justify-content-between mt-4">
+                    <p class="restaurant-vat">P. IVA : <br> {{ $restaurant->vat_number }}</p>
+                    <p class="restaurant-address">Indirizzo : <br> {{ $restaurant->address }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection

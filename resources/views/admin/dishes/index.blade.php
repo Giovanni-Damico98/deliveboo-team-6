8fc1de9a8d655ae668143f6ddb1ce8b0247ab76a
Giovanni-Damico98 <gd.installazioni25@gmail.com>
2024-12-04 14:59:42 +0100

    style for index.blade.php

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -1,39 +1,46 @@
-@extends("layouts.admin")
+@extends('layouts.admin')
+@vite('resources/css/dishes/index.css')
 
-@section("content")
+@section('content')
+    <div class="container-fluid mt-1 w-100 d-flex justify-content-center flex-column align-items-center">
+        <div class="new-dish d-flex w-100 justify-content-center mb-3">
+            <a href="{{ route('admin.dishes.create') }}" class="btn btn-primary m-3 w-25">
+                Crea un nuovo piatto
+            </a>
+        </div>
+        <div class="row mb-2 w-75">
+            @forelse ($dishes as $index => $dish)
+                <div class="col-md-4">
+                    <div class="card card-overlay" style="background-image: url('{{ asset($dish->image) }}');">
+                        <div class="card-body">
+                            <h2 class="card-title text-white">{{ $dish->name }}</h2>
+                            <p class="card-text text-white">
+                                {{ substr($dish->description, 0, 60) . (strlen($dish->description) > 50 ? '...' : '') }}
+                            </p>
 
-<div class="container mt-5">
-    <a href="{{ route("admin.dishes.create") }}" class="btn btn-primary m-3 d-flex justify-content-center align-content-center">
-        Crea un nuovo piatto
-    </a>
-    <div class="row mb-2 g-3">
-        @forelse ($dishes as $index => $dish )
-            <div class="col-md-4">
-                <div class="card" style="width: 18rem;">
-                    <img src="{{$dish->image}}" class="card-img-top" alt="{{ $dish->name }}">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $dish->name }}</h5>
-                        <p class="card-text">Descrizione: {{ $dish->description }}</p>
-                        <p class="card-text"><strong> Prezzo: $ {{ $dish->price }} </strong></p>
-                        <a class="btn btn-secondary mb-2" href="{{ route("admin.dishes.show", $dish) }}">
-                            Guarda il piatto
-                        </a>
-                        <a class="btn btn-secondary mb-2" href="{{ route("admin.dishes.edit", $dish) }}">
-                            Modifica il piatto
-                        </a>
-                        <form action="{{ route("admin.dishes.delete", $dish) }}" method="post">
-                            @csrf
-                            @method("DELETE")
-                            <input type="submit" value="Elimina" class="btn btn-danger mb-2">
-                        </form>
+                            <p class="card-text text-end text-white"><strong> Prezzo: €{{ $dish->price }} </strong></p>
+                            <div class="action-div w-100">
+                                <a class="btn btn-primary mb-2 w-100" href="{{ route('admin.dishes.show', $dish) }}">
+                                    Guarda il piatto
+                                </a>
+                                <a class="btn btn-warning mb-2 w-100" href="{{ route('admin.dishes.edit', $dish) }}">
+                                    Modifica il piatto
+                                </a>
+                                <form action="{{ route('admin.dishes.delete', $dish) }}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+                                    <input type="submit" value="Elimina" class="btn btn-danger mb-2 w-100">
+                                </form>
+                            </div>
+                        </div>
+                        <div class="card-overlay"></div>
                     </div>
                 </div>
-            </div>
-        @empty
-            <div>
-                <h1>Non ci sono piatti</h1>
-            </div>
-        @endforelse
+            @empty
+                <div>
+                    <h1>Non ci sono piatti</h1>
+                </div>
+            @endforelse
+        </div>
     </div>
-</div>
 @endsection

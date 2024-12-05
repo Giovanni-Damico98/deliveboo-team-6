ce1e32d4086cb25f8f9646e6d37b954289e7cd9c
FrancescoMinichiello <fraps2278@gmail.com>
2024-12-05 12:44:06 +0100

    Added table in index.blade

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -2,45 +2,62 @@
 @vite('resources/css/dishes/index.css')
 
 @section('content')
-    <div class="container-fluid mt-1 w-100 d-flex justify-content-center flex-column align-items-center">
-        <div class="new-dish d-flex w-100 justify-content-center mb-3">
-            <a href="{{ route('admin.dishes.create') }}" class="btn btn-primary m-3 w-25">
+    <div class="container-fluid d-flex justify-content-center flex-column align-items-center">
+        <div class="d-flex w-100 justify-content-center">
+            <a href="{{ route('admin.dishes.create') }}" class="btn btn-success p-2 w-25 fs-2">
                 Crea un nuovo piatto
             </a>
         </div>
         <div class="row mb-2 w-75">
-            @forelse ($dishes as $index => $dish)
-                <div class="col-md-4">
-                    <div class="card card-overlay mb-4" style="background-image: url('{{ asset($dish->image) }}');">
-                        <div class="card-body">
-                            <h2 class="card-title text-white">{{ $dish->name }}</h2>
-                            <p class="card-text text-white">
-                                {{ substr($dish->description, 0, 60) . (strlen($dish->description) > 50 ? '...' : '') }}
-                            </p>
+            <div class="container my-5">
+                <div
+                    class="header-of-table col d-flex align-items-center p-4 justify-content-center text-white rounded-top-5">
+                    <h1 class="m-0">Gestisci i tuoi piatti</h1>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <table class="table table-dark table-striped">
+                            <thead>
+                                <tr>
+                                    <th scope="col">ID</th>
+                                    <th scope="col">Nome</th>
+                                    <th scope="col">Descrizione</th>
+                                    <th scope="col">Prezzo</th>
+                                    <th scope="col">Azioni</th>
 
-                            <p class="card-text text-end text-white"><strong> Prezzo: €{{ $dish->price }} </strong></p>
-                            <div class="action-div w-100">
-                                <a class="btn btn-primary mb-2 w-100" href="{{ route('admin.dishes.show', $dish) }}">
-                                    Guarda il piatto
-                                </a>
-                                <a class="btn btn-warning mb-2 w-100" href="{{ route('admin.dishes.edit', $dish) }}">
-                                    Modifica il piatto
-                                </a>
-                                <form action="{{ route('admin.dishes.delete', $dish) }}" method="post">
-                                    @csrf
-                                    @method('DELETE')
-                                    <input type="submit" value="Elimina" class="btn btn-danger mb-2 w-100">
-                                </form>
-                            </div>
-                        </div>
-                        <div class="card-overlay"></div>
+                                </tr>
+                            </thead>
+                            @forelse ($dishes as $index => $dish)
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">{{ $dish->id }}</th>
+                                        <td>{{ $dish->name }}</td>
+                                        <td>{{ substr($dish->description, 0, 60) . (strlen($dish->description) > 50 ? '...' : '') }}
+                                        </td>
+                                        <td>{{ $dish->price }}</td>
+                                        <td>
+                                            <a href="{{ route('admin.dishes.edit', $dish) }}"><button
+                                                    class="bg-warning p-2 rounded-2 fw-semibold border-0 mb-1">Edit</button></a>
+                                            <form class="mb-0" action="{{ route('admin.dishes.delete', $dish) }}"
+                                                method="post">
+                                                @csrf
+                                                @method('DELETE')
+                                                <input type="submit" value="Elimina"
+                                                    class="btn btn-danger p-2 rounded-2 fw-semibold border-0 text-black">
+                                            </form>
+                                        </td>
+
+                                    </tr>
+                                </tbody>
+                            @empty
+                                <div>
+                                    <h1>Non ci sono piatti</h1>
+                                </div>
+                            @endforelse
+                        </table>
                     </div>
                 </div>
-            @empty
-                <div>
-                    <h1>Non ci sono piatti</h1>
-                </div>
-            @endforelse
+            </div>
         </div>
     </div>
 @endsection

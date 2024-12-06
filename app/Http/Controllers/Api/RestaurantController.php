8a401222fc6ec9d9411cc64557e39b35c8a92a9f
AngeloCrisci <angelocrisci1998@gmail.com>
2024-12-06 10:27:37 +0100

    Start to create api

---
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Restaurant;
+use Illuminate\Http\Request;
+
+class RestaurantController extends Controller
+{
+    // metodo index per creare l'end point
+    public function index(){
+        $restaurants = Restaurant::all();
+
+        return response()->json(
+            [
+                "success" => true,
+                "results" => $restaurants,
+
+            ]
+            );
+    }
+
+}

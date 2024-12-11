a3c4de9f0d34648d2d19b7bbf77ba81f5c821fc1
AngeloCrisci <angelocrisci1998@gmail.com>
2024-12-11 12:36:15 +0100

    First try to implement email sender

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,12 @@
 use App\Http\Controllers\Api\RestaurantController;
 use App\Http\Controllers\Api\DishController;
 use App\Http\Controllers\Api\CategoryController;
+use App\Mail\EmailNotification;
 use App\Models\Category;
+use App\Models\Restaurant;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -30,3 +34,28 @@ Route::get("/category" , [CategoryController::class , "index"])->name("api.categ
 
 // Rotta per l'esposizione dell'api per dishes
 Route::get("/dishes", [DishController::class, "index"])->name("api.dishes.index");
+
+// Rotta per l'invio di e-mail
+Route::post('/send-email' , function (Request $request){
+    $restaurant = Restaurant::all();
+    $details = $request->validate([
+        'title' => 'required|string',
+        'body' => 'required|string',
+        'to' => 'required|email',
+    ]);
+
+    // Recupero l'email dal database in modo dinamico
+    $databaseEmail = User::where('id')->value('email');
+
+    // Invia email al destinatario inserito nel form
+    Mail::to($details['to'])->send(new EmailNotification([
+        'title' => $details['details'],
+        'body' => "Il tuo ordine presso il {{$restaurant->name}} è stato confermato "
+    ]));
+
+    //Invia email all'indirizzo del database
+    Mail::to($databaseEmail)->send(new EmailNotification([
+        'title' => $details['title'],
+        'body' => "Hai ricevuto un ordine"
+    ]));
+});
